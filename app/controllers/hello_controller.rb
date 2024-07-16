349f56dc99bcaf4a10c15922ecadd5b89663df5e
YutoNakamitsu <54886961+uutann@users.noreply.github.com>
2024-07-16 04:26:34 +0000

    create hello'godbay

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,11 @@
 class HelloController < ApplicationController
-  def index
+  def index; end
+
+  def hello
+    render html: 'hello, world!!!!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
   end
 end
